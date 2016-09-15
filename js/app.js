7bc50b47fa5494140c8d7c335e9e08f83cd542f6
debu <rony.debu@gmail.com>
2016-09-15 18:29:32 +0530

    Beautify the js

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = GenerateRandom(0,400);
-    this.y = GenerateRandom(10,240);
-    this.step = GenerateRandom(0,50);
+    this.x = GenerateRandom(0, 400);
+    this.y = GenerateRandom(10, 240);
+    this.step = GenerateRandom(0, 50);
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -16,11 +16,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += dt*this.step;
+    this.x += dt * this.step;
     if (this.x > ctx.canvas.width) {
-      this.destroy();
-      RemoveEnemy();
-      InitializeEnemy();
+        this.destroy();
+        RemoveEnemy();
+        InitializeEnemy();
     }
     //this.y += dt;
 };
@@ -30,70 +30,70 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Enemy.prototype.destroy = function(){
-  this.delete = true;
-}
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(){
-    this.sprite='images/char-boy.png';
-    this.x = 400;
-    this.y = 400;
-    this.stepX = 0;
-    this.stepY = 0;
-}
-/*Update Player's movement*/
-Player.prototype.update = function(){
-    this.x += this.check('x');//checks whether we are inside the boundary for x
-    this.y += this.check('y');//checks whether we are inside the boundary for y
-    this.stepX = 0;
-    this.stepY = 0;
-}
-/*Check whether movement in a particular direction possible*/
-Player.prototype.check =function(direction){
-    var ret=0;
-    switch (direction.toLowerCase()) {
-      case 'x':
-        var nextStep = this.x + this.stepX;
-        ret = (nextStep < 0 || nextStep >  ctx.canvas.width) ? 0 : this.stepX;
-        break;
-      case 'y':
-        nextStep = this.x + this.stepY;
-        ret = (nextStep < 0 || nextStep >  ctx.canvas.height) ? 0 : this.stepX;
-        break;
-      default:
-
+Enemy.prototype.destroy = function() {
+        this.delete = true;
     }
-    return ret;
-}
-/*Render the player*/
-Player.prototype.render = function(){
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-/*Takes the input for the player movement*/
-Player.prototype.handleInput = function(key){
+    // Now write your own player class
+    // This class requires an update(), render() and
+    // a handleInput() method.
+var Player = function() {
+        this.sprite = 'images/char-boy.png';
+        this.x = 400;
+        this.y = 400;
+        this.stepX = 0;
+        this.stepY = 0;
+    }
+    /*Update Player's movement*/
+Player.prototype.update = function() {
+        this.x += this.check('x'); //checks whether we are inside the boundary for x
+        this.y += this.check('y'); //checks whether we are inside the boundary for y
+        this.stepX = 0;
+        this.stepY = 0;
+    }
+    /*Check whether movement in a particular direction possible*/
+Player.prototype.check = function(direction) {
+        var ret = 0;
+        switch (direction.toLowerCase()) {
+            case 'x':
+                var nextStep = this.x + this.stepX;
+                ret = (nextStep < 0 || nextStep > ctx.canvas.width) ? 0 : this.stepX;
+                break;
+            case 'y':
+                nextStep = this.x + this.stepY;
+                ret = (nextStep < 0 || nextStep > ctx.canvas.height) ? 0 : this.stepX;
+                break;
+            default:
+
+        }
+        return ret;
+    }
+    /*Render the player*/
+Player.prototype.render = function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    /*Takes the input for the player movement*/
+Player.prototype.handleInput = function(key) {
     if (!key) {
-      return;
+        return;
     }
     switch (key.toLowerCase()) {
-      case 'left':
-        this.stepX = -10;
-        this.stepY = 0;
-        break;
-      case 'right':
-        this.stepX = 10;
-        this.stepY = 0;
-        break;
-      case 'up':
-        this.stepX = 0;
-        this.stepY = -10;
-        break;
-      case 'down':
-        this.stepX = 0;
-        this.stepY = 10;
-        break;
-      default:
+        case 'left':
+            this.stepX = -10;
+            this.stepY = 0;
+            break;
+        case 'right':
+            this.stepX = 10;
+            this.stepY = 0;
+            break;
+        case 'up':
+            this.stepX = 0;
+            this.stepY = -10;
+            break;
+        case 'down':
+            this.stepX = 0;
+            this.stepY = 10;
+            break;
+        default:
 
     }
 }
@@ -102,7 +102,7 @@ Player.prototype.handleInput = function(key){
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var player = new Player();
-var allEnemies=[InitializeEnemy(),InitializeEnemy(),InitializeEnemy()];
+var allEnemies = [InitializeEnemy(), InitializeEnemy(), InitializeEnemy()];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -120,23 +120,23 @@ document.addEventListener('keyup', function(e) {
 /*Custom Functions for usage*/
 
 /*To generate random positions for enemy and other similar features*/
-function GenerateRandom(start,limit){
-  return  Math.floor(Math.random() * limit) + start;
+function GenerateRandom(start, limit) {
+    return Math.floor(Math.random() * limit) + start;
 }
 
 /*Initializing new enemy*/
-function InitializeEnemy(){
-  var enemy = new Enemy();
-  allEnemies.push(enemy);
+function InitializeEnemy() {
+    var enemy = new Enemy();
+    allEnemies.push(enemy);
 }
 
 /*Removing an enemy*/
-function RemoveEnemy(){
-  var index;
-  allEnemies.forEach(function(item, i){
-    if(this.delete){
-      index = i;
-    }
-  });
-  allEnemies.splice(index,1);
+function RemoveEnemy() {
+    var index;
+    allEnemies.forEach(function(item, i) {
+        if (this.delete) {
+            index = i;
+        }
+    });
+    allEnemies.splice(index, 1);
 }
