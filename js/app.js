9d56df259e00d407b6b5ebecbf6c48d2ef0b70fc
debu <rony.debu@gmail.com>
2016-09-25 13:59:33 +0530

    Randomize enemy generation

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    this.x = GenerateRandom(0, 400);
+    /*
+    *   Randomizing the generation of the position of the enemy
+    */
+    this.x = GenerateRandom(0, 200);
     this.y = GenerateRandom(10, 240);
-    this.step = GenerateRandom(0, 50);
+    /*  Step tells that how fast the enemy travels along
+    *   x-axis
+    */
+    this.step = GenerateRandom(150, 200);
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -17,12 +21,15 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += dt * this.step;
+    /*
+    * Destroying the enemy when it crosses the
+    * canvas width
+    */
     if (this.x > ctx.canvas.width) {
-        this.destroy();
-        RemoveEnemy();
-        InitializeEnemy();
+        this.destroy(); //  Marks the enemy to be deleted
+        RemoveEnemy();  //  Removes the enemy from the allEnemies array
     }
-    //this.y += dt;
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -30,80 +37,147 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+/*
+*   To destroy an enemy
+*   delete property is added
+*   is marked true
+*/
 Enemy.prototype.destroy = function() {
-        this.delete = true;
-    }
-    // Now write your own player class
-    // This class requires an update(), render() and
-    // a handleInput() method.
+    this.delete = true;
+}
+
+/*
+*   Get current position of the enemy
+*/
+Enemy.prototype.position = function() {
+    return {x: this.x, y: this.y};
+}
+
+
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
 var Player = function() {
         this.sprite = 'images/char-boy.png';
-        this.x = 400;
+        this.x = 200;
         this.y = 400;
         this.stepX = 0;
         this.stepY = 0;
     }
-    /*Update Player's movement*/
+/*
+*   Update Player's movement
+*/
 Player.prototype.update = function() {
         this.x += this.check('x'); //checks whether we are inside the boundary for x
         this.y += this.check('y'); //checks whether we are inside the boundary for y
         this.stepX = 0;
         this.stepY = 0;
     }
-    /*Check whether movement in a particular direction possible*/
+
+/*
+*   Check whether movement in a particular direction possible
+*/
 Player.prototype.check = function(direction) {
         var ret = 0;
         switch (direction.toLowerCase()) {
             case 'x':
                 var nextStep = this.x + this.stepX;
-                ret = (nextStep < 0 || nextStep > ctx.canvas.width) ? 0 : this.stepX;
+                var width = ctx.canvas.width - 100;
+                ret = (nextStep < 0 || nextStep >= width) ? 0 : this.stepX;
                 break;
             case 'y':
-                nextStep = this.x + this.stepY;
-                ret = (nextStep < 0 || nextStep > ctx.canvas.height) ? 0 : this.stepX;
+                nextStep = this.y + this.stepY;
+                var heit = ctx.canvas.height * 0.7;
+                ret = (nextStep < 0 || nextStep >= heit) ? 0 : this.stepY;
                 break;
             default:
 
         }
         return ret;
     }
-    /*Render the player*/
+
+/*
+*   Render the player
+*/
 Player.prototype.render = function() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-    /*Takes the input for the player movement*/
+
+/*
+*   Takes the input for the player movement
+*/
 Player.prototype.handleInput = function(key) {
     if (!key) {
         return;
     }
+    var steps = this.steps();
     switch (key.toLowerCase()) {
         case 'left':
-            this.stepX = -10;
+            this.stepX = -1*steps;
             this.stepY = 0;
             break;
         case 'right':
-            this.stepX = 10;
+            this.stepX = 1*steps;
             this.stepY = 0;
             break;
         case 'up':
             this.stepX = 0;
-            this.stepY = -10;
+            this.stepY = -1*steps;
             break;
         case 'down':
             this.stepX = 0;
-            this.stepY = 10;
+            this.stepY = 1*steps;
             break;
         default:
 
     }
 }
+/*
+*   Get current Player position
+*/
+Player.prototype.position = function() {
+    return {x: this.x, y: this.y};
+}
+/*
+*   Default Step, the speed the player moves
+*/
+Player.prototype.steps = function(){
+  return 50;
+}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var player = new Player();
-var allEnemies = [InitializeEnemy(), InitializeEnemy(), InitializeEnemy()];
 
+/*Global values to be accessed across the app*/
+var player;
+var allEnemies = [];
+var clear;
+
+/*
+*   This function is called the first time
+*   when the game is initialized, thus
+*   initializing the player object and the
+*   allEnemies array with the enemies
+*/
+function Start(){
+    // Now instantiate your objects.
+    // Place all enemy objects in an array called allEnemies
+    // Place the player object in a variable called player
+    player = new Player();
+    allEnemies = [];
+    InitializeEnemy();
+    InitializeEnemy();
+    InitializeEnemy();
+    clear = setInterval(function(){
+      InitializeEnemy();
+    },500);
+}
+
+/*
+*   Clear enemy generation
+*/
+function ClearEnemy(){
+  clearInterval(clear);
+}
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
